b0e5f0f845cb809145c7e96720ec291fa7ba38ae
Saiharish326 <kmsaiharish@gmail.com>
2024-06-18 01:25:16 +0530

    changes made

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,33 @@
 import { useState } from "react"
 import Cell from "./components/cell"
 const App =() => {
-  const [cells, setCells]=useState(["onnu","","","","","","","",""])
+  const [cells, setCells]=useState(["","","","","","","","",""])
+  const [go, setGo]=useState("circle")
+  const [winningMessage, setWinningMessage]=useState(null)
+  const message="it is now "+go+"'s turn"
+  const checkScore = () => {
+    const winningCombos = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+    ]
+  }
   return (
     <div className="app">
       <div className="gameboard">
-        {cells.map(cell => <Cell cell={cell}/>)}
+        {cells.map((cell,index) =>
+          <Cell 
+            key={index}
+            id={index}
+            cell={cell}
+            go={go}
+            setGo={setGo}
+            cells={cells}
+            setCells={setCells}
+          />
+        )}
       </div>
+        <p>{winningMessage || message}</p>
       
     </div>
   )
